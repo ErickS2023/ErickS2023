8831d6d33b43e14ceec9c3939c0fecf59e4d8c2f
ErickS2023 <141955911+ErickS2023@users.noreply.github.com>
2023-08-10 12:40:17 -0600

    Clase de algoritmos
    3 generaciones de computadoras

---
+++ 3 generaciones de las computadoras.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main (int argc,char **argv)
+{
+	printf("Primera generacion de computadoras"); "\n"
+	
+	printf("¿En que años fueron desarrolladas estas computadoras"); "\n"
+	
+	printf("De 1940 a 1950");  "\n"
+	
+	printf("Principales caracteristicas de estas computadoras"); "\n"
+	
+	printf("Utilizaban valvulas electronicas principlmente diseñadas para calculos cientificos y militares"); "\n"
+	
+	return 0;
+}
+
+#include <stdio.h>
+
+int main (int argc,char **argv)
+{
+	printf("Segunda generacion de computadoras"); "\n"
+	
+	printf("¿En que años fueron desarrolladas estas computadoras"); "\n"
+	
+	printf("De 1950 a 1960");  "\n"
+	
+	printf("Principales caracteristicas de estas computadoras"); "\n"
+	
+	printf("Utilizaban  transistores en lugar de tubos de vacio por lo que eran pequeñas rapidas y confiables"); "\n"
+	
+	return 0;
+}
+
+#include<stdio.h>
+
+int main (int argc,char **argv)
+{
+	printf("Tercera generacion de computadoras"); "\n"
+	
+	printf("¿En que años fueron desarrolladas estas computadoras"); "\n"
+	
+	printf("De 1960 a 1970");  "\n"
+	
+	printf("Principales caracteristicas de estas computadoras"); "\n"
+	
+	printf("se utilizaron circuitos integrados permitio un aumento significativo en la velocidad"); "\n"
+	
+	return 0;
+}
+
+
